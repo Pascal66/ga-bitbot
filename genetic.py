e33fc03883a81b553f5891f22e699d9109e206b8
emfb <emfb@monolith.(none)>
2011-07-26 01:59:54 -0400

    Added GPL licence headers to source files

--- genetic.py
+++ genetic.py
@@ -1,3 +1,25 @@
+
+"""
+genetic v0.01 
+
+Copyright 2011 Brian Monkaba
+
+This file is part of ga-bitbot.
+
+    ga-bitbot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ga-bitbot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ga-bitbot.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import random
 import time
 from operator import itemgetter
--- gts.py
+++ gts.py
@@ -1,5 +1,28 @@
 
-#genetic trade simulator
+"""
+gts v0.01 
+
+genetic trade simulator
+
+Copyright 2011 Brian Monkaba
+
+This file is part of ga-bitbot.
+
+    ga-bitbot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ga-bitbot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ga-bitbot.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 
 
 
