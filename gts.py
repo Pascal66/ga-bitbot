cb23b97839d582b08532c7c7ed37f63334e9b243
emfb <emfb@monolith.(none)>
2011-08-12 19:02:14 -0400

    fixed mixed tab/spaces issue with gts.py

--- gts.py
+++ gts.py
@@ -51,169 +51,167 @@ import pdb
 import time
 
 if __name__ == "__main__":
-    __appversion__ = "0.01a"
-    print "Genetic Bitcoin Trade Simulator v%s"%__appversion__
+	__appversion__ = "0.01a"
+	print "Genetic Bitcoin Trade Simulator v%s"%__appversion__
+
+	max_length = 60 * 24 * 360
+	calibrate = 1	#set to one to adjust the population size to maintain a one min test cycle
+
+	def load():
+		#open the history file
+		#print "loading the data set"
+		f = open("./datafeed/bcfeed_mtgoxUSD_1min.csv",'r')
+		#f = open("./datafeed/test_data.csv",'r')
+		d = f.readlines()
+		f.close()
+	
+		if len(d) > max_length:
+			#truncate the dataset
+			d [max_length * -1:]
+
+		#load the backtest dataset
+		input = []
+		for row in d[1:]:
+			r = row.split(',')[1] #last price
+			t = row.split(',')[0] #time
+			input.append([int(float(t)),float(r)])
+		#print "done loading"
+		return input
+	
+	#load the inital data
+	input = load()
     
-    max_length = 60 * 24 * 360
-    calibrate = 1	#set to one to adjust the population size to maintain a one min test cycle
+	#configure the gene pool
+	g = genepool()
+	g = load_config_into_object(load_config_from_file("gene_def.json"),g)
 
-    def load():
-	#open the history file
-	#print "loading the data set"
-	f = open("./datafeed/bcfeed_mtgoxUSD_1min.csv",'r')
-	#f = open("./datafeed/test_data.csv",'r')
-	d = f.readlines()
-	f.close()
+	#g.set_log("winners.txt")
+	print "Creating the trade engine"
+	te = trade_engine()
+	te.score_only = True
+	#preprocess input data
+	te.classify_market(input)
+
+	#process command line args
+	quartile = ''
+	bs = ''
+	print sys.argv
+	if len(sys.argv) == 3:
+		# Convert the two arguments from strings into numbers
+		quartile = sys.argv[1]
+		bs = sys.argv[2]
 	
-	if len(d) > max_length:
-		#truncate the dataset
-		d [max_length * -1:]
-
-	#load the backtest dataset
-	input = []
-	for row in d[1:]:
-	    r = row.split(',')[1] #last price
-	    t = row.split(',')[0] #time
-	    input.append([int(float(t)),float(r)])
-	#print "done loading"
-	return input
-	
-    #load the inital data
-    input = load()
-    
-    #configure the gene pool
-    g = genepool()
-    g = load_config_into_object(load_config_from_file("gene_def.json"),g)
-
-    #g.set_log("winners.txt")
-    print "Creating the trade engine"
-    te = trade_engine()
-    te.score_only = True
-    #preprocess input data
-    te.classify_market(input)
-
-    #process command line args
-    quartile = ''
-    bs = ''
-    print sys.argv
-    if len(sys.argv) == 3:
-    	# Convert the two arguments from strings into numbers
-    	quartile = sys.argv[1]
-    	bs = sys.argv[2]
-	
-    #which quartile group to test
-    while not (quartile in ['1','2','3','4']):
-    	print "Which quartile group to test? (1,2,3,4):"
-	quartile = raw_input()
-    quartile = int(quartile)
+	#which quartile group to test
+	while not (quartile in ['1','2','3','4']):
+		print "Which quartile group to test? (1,2,3,4):"
+		quartile = raw_input()
+	quartile = int(quartile)
     
 
-    #bootstrap the population with the winners available from the gene_pool server
-    while not(bs == 'y' or bs == 'n'):
-    	print "Bootstrap from the gene_server? (y/n)"
-    	bs = raw_input()
-    if bs == 'y':
-	bob_simulator = True
-	g.local_optima_trigger = 10
-	calibrate = 1
-    	bootstrap_bobs = json.loads(server.get_bobs(quartile))
-	bootstrap_all = json.loads(server.get_all(quartile))
-	print len(bootstrap_all)
-	if (type(bootstrap_bobs) == type([])) and (type(bootstrap_all) == type([])):
-		g.seed()
-		g.pool = []		
-		g.insert_genedict_list(bootstrap_bobs)
-		g.insert_genedict_list(bootstrap_all)	
-		g.reset_scores()
-	else: #if no BOBS or high scores..seed with a new population
-		print "no BOBs or high scores available...seeding new pool."
-		g.seed()
-
-	print "%s BOBs loaded"%len(bootstrap_bobs)
-	print "%s high scores loaded"%len(bootstrap_all)
-
-	print "Pool size: %s"%len(g.pool)
-	
-    else:
-	bob_simulator = False
-	g.local_optima_trigger = 5
-	print "Seeding the initial population"
-	g.seed()
-
-    cycle_time = 20 * 1#time in seconds to test the entire population
-    test_count = 0
-    total_count = 0
-    max_score = -10000
-    max_score_id = -1
-    start_time = time.time()
-    print "Running the simulator"
-    while 1:
-	#print test_count #debug 
-	#periodicaly reload the data set
-	test_count += 1
-	total_count += 1
-
-	    
-	if total_count%g.pool_size == 0:
-	    #benchmark the cycle speed
-	    current_time = time.time()
-	    elapsed_time = current_time - start_time
-	    gps = total_count / elapsed_time
-	    if calibrate == 1:
-	    	g.pool_size = int(gps * cycle_time)
-	    print "Genes/Sec Processed: ","%.2f"%gps,"Pool Size: ",g.pool_size,"Total Processed: ",total_count
-	    #load the latest trade data
-	    #print "Loading the lastest trade data..."
-	    input = load()
-	    #preprocess input data
-	    te.classify_market(input)
-	    #print g.local_optima_reached
-	    if g.local_optima_reached:
-		#print '#'*10, " Local optima reached...sending bob to the gene_server ", '#'*10		
-		max_score = 0
-		test_count = 0
-		g.max_mutate = 0.5
-		g.prune_threshold = 0.2
-
-		max_gene = g.get_by_id(max_score_id)
-		if max_gene != None:
-		    print "--\tSubmit BOB for id:%s to server (%.2f)"%(str(max_gene['id']),max_gene['score'])
-		    server.put_bob(json.dumps(max_gene),quartile)
-	    	if bob_simulator:
-			#after a local optima is reached, sleep for some time to allow extra processing power to
-			#the other clients so they can find potentialy better genes
-			#print "going to sleep..."
-			#time.sleep(60*15)
-    			bootstrap_bobs = json.loads(server.get_bobs(quartile))
-		    	bootstrap_all = json.loads(server.get_all(quartile))
-			if (type(bootstrap_bobs) == type([])) and (type(bootstrap_all) == type([])):
-				g.seed()
-				g.pool = []		
-				g.insert_genedict_list(bootstrap_bobs)
-				g.insert_genedict_list(bootstrap_all)	
-				g.reset_scores()			
-			else: #if no BOBS or high scores..seed with a new population
-				#print "no BOBs or high scores available...seeding new pool."
-				g.seed() #not sure if I need this
-		else:
-			#print "slicing the gene pool"
-			#g.pool = g.pool[int(g.pool_size * 70):]
-			g.pool = []
+	#bootstrap the population with the winners available from the gene_pool server
+	while not(bs == 'y' or bs == 'n'):
+		print "Bootstrap from the gene_server? (y/n)"
+		bs = raw_input()
+	if bs == 'y':
+		bob_simulator = True
+		g.local_optima_trigger = 10
+		calibrate = 1
+		bootstrap_bobs = json.loads(server.get_bobs(quartile))
+		bootstrap_all = json.loads(server.get_all(quartile))
+		print len(bootstrap_all)
+		if (type(bootstrap_bobs) == type([])) and (type(bootstrap_all) == type([])):
 			g.seed()
-			g.local_optima_reached = False
-			#g.local_optima_buffer = []
-		
-	    #print "back to crunching..."
+			g.pool = []		
+			g.insert_genedict_list(bootstrap_bobs)
+			g.insert_genedict_list(bootstrap_all)	
+			g.reset_scores()
+		else: #if no BOBS or high scores..seed with a new population
+			print "no BOBs or high scores available...seeding new pool."
+			g.seed()
+
+		print "%s BOBs loaded"%len(bootstrap_bobs)
+		print "%s high scores loaded"%len(bootstrap_all)
+
+		print "Pool size: %s"%len(g.pool)
+	
+	else:
+		bob_simulator = False
+		g.local_optima_trigger = 5
+		print "Seeding the initial population"
+		g.seed()
+
+	cycle_time = 20 * 1#time in seconds to test the entire population
+	test_count = 0
+	total_count = 0
+	max_score = -10000
+	max_score_id = -1
+	start_time = time.time()
+	print "Running the simulator"
+	while 1:
+		#print test_count #debug 
+		#periodicaly reload the data set
+		test_count += 1
+		total_count += 1
+
+		    
+		if total_count%g.pool_size == 0:
+			#benchmark the cycle speed
+			current_time = time.time()
+			elapsed_time = current_time - start_time
+			gps = total_count / elapsed_time
+			if calibrate == 1:
+				g.pool_size = int(gps * cycle_time)
+			print "Genes/Sec Processed: ","%.2f"%gps,"Pool Size: ",g.pool_size,"Total Processed: ",total_count
+			#load the latest trade data
+			#print "Loading the lastest trade data..."
+			input = load()
+			#preprocess input data
+			te.classify_market(input)
+			#print g.local_optima_reached
+		if g.local_optima_reached:
+			#print '#'*10, " Local optima reached...sending bob to the gene_server ", '#'*10		
+			max_score = 0
+			test_count = 0
+			g.max_mutate = 0.5
+			g.prune_threshold = 0.2
+
+			max_gene = g.get_by_id(max_score_id)
+			if max_gene != None:
+				print "--\tSubmit BOB for id:%s to server (%.2f)"%(str(max_gene['id']),max_gene['score'])
+			server.put_bob(json.dumps(max_gene),quartile)
+			if bob_simulator:
+				#after a local optima is reached, sleep for some time to allow extra processing power to
+				#the other clients so they can find potentialy better genes
+				#print "going to sleep..."
+				#time.sleep(60*15)
+				bootstrap_bobs = json.loads(server.get_bobs(quartile))
+			    	bootstrap_all = json.loads(server.get_all(quartile))
+				if (type(bootstrap_bobs) == type([])) and (type(bootstrap_all) == type([])):
+					g.seed()
+					g.pool = []		
+					g.insert_genedict_list(bootstrap_bobs)
+					g.insert_genedict_list(bootstrap_all)	
+					g.reset_scores()			
+				else: #if no BOBS or high scores..seed with a new population
+					#print "no BOBs or high scores available...seeding new pool."
+					g.seed() #not sure if I need this
+			else:
+				#print "slicing the gene pool"
+				#g.pool = g.pool[int(g.pool_size * 70):]
+				g.pool = []
+				g.seed()
+				g.local_optima_reached = False
+				#g.local_optima_buffer = []
 
 	if test_count > (g.pool_size * 3):
-	    test_count = 0
-	    print "Reset scores to force retest of winners..."
-	    test_count = 0
-	    max_score = 0	#knock the high score down to prevent blocking
+		test_count = 0
+		print "Reset scores to force retest of winners..."
+		test_count = 0
+		max_score = 0	#knock the high score down to prevent blocking
 				#latest scoring data which may fall due to
 				#the latest price data
-	    g.next_gen()
-	    g.reset_scores()
+		g.next_gen()
+		g.reset_scores()
 
 	#create/reset the trade engine
 	te.reset()
@@ -240,27 +238,27 @@ if __name__ == "__main__":
 	te.test_quartile(quartile)
 
 	try:
-	    for i in input:
-		te.input(i[0],i[1])
+		for i in input:
+			te.input(i[0],i[1])
 	except:
-	    #kill off any genes that crash the trade engine (div by 0 errors for instance)
-	    g.set_score(ag['id'],g.kill_score)
+		#kill off any genes that crash the trade engine (div by 0 errors for instance)
+		g.set_score(ag['id'],g.kill_score)
 	else:
-	    #return the score to the gene pool
-	    score = te.score()
-	    g.set_score(ag['id'],score)
-	    g.set_message(ag['id'],"Balance: " + str(te.balance) +"; Wins: " + str(te.wins)+ "; Loss:" + str(te.loss) +  "; Positions: " + str(len(te.positions)))
-	
-	    #if a new high score is found (or revisited) submitt the gene to
-	    #the server
-	    if score > max_score:
-		print "--\tSubmit high score for id:%s to server (%.2f)"%(str(ag['id']),score)
-		max_score = score
-		max_score_id = ag['id']
-		max_gene = g.get_by_id(max_score_id)
-		if max_gene != None:
-		    server.put(json.dumps(max_gene),quartile)
-		else:
-		    print "MAX_GENE is None!!"
-		 
+		#return the score to the gene pool
+		score = te.score()
+		g.set_score(ag['id'],score)
+		g.set_message(ag['id'],"Balance: " + str(te.balance) +"; Wins: " + str(te.wins)+ "; Loss:" + str(te.loss) +  "; Positions: " + str(len(te.positions)))
+
+		#if a new high score is found (or revisited) submitt the gene to
+		#the server
+		if score > max_score:
+			print "--\tSubmit high score for id:%s to server (%.2f)"%(str(ag['id']),score)
+			max_score = score
+			max_score_id = ag['id']
+			max_gene = g.get_by_id(max_score_id)
+			if max_gene != None:
+				server.put(json.dumps(max_gene),quartile)
+			else:
+				print "MAX_GENE is None!!"
+
 
