0a4121859440a8a16661ae93f7bb2b77e6fe1da5
emfb <emfb@monolith.(none)>
2011-09-25 14:27:11 -0400

    added MtGoxHMAC.py to the project which implements the HMAC authentication method. This will replace the outdated MtGoxAPI.py module.

---
+++ MtGoxHMAC.py
@@ -0,0 +1,180 @@
+from contextlib import closing
+from Crypto.Cipher import AES
+import base64
+import hmac
+import hashlib
+import time
+import json
+import urllib
+import urllib2
+import urlparse
+
+class ServerError(Exception):
+	def __init__(self, ret):
+		self.ret = ret
+	def __str__(self):
+		return "Server error: %s" % self.ret
+class UserError(Exception):
+	def __init__(self, errmsg):
+		self.errmsg = errmsg
+	def __str__(self):
+		return self.errmsg
+
+class Client:
+	def __init__(self, enc_password=""):
+		if enc_password == "":
+			print "MtGoxHMAC: Enter your API key file encryption password:"
+			enc_password = raw_input()
+		try:	
+			f = open('./config/salt.txt','r')
+			salt = f.read()
+			f.close()
+			hash_pass = hashlib.sha256(enc_password + salt).digest()
+
+			f = open('./config/api_key.txt')
+			ciphertext = f.read()
+			f.close()
+			decryptor = AES.new(hash_pass, AES.MODE_CBC)
+			plaintext = decryptor.decrypt(ciphertext)
+			d = json.loads(plaintext)
+			self.key = d['key']
+			self.secret = d['secret']
+		except:
+			print "\n\n\nError: you may have entered an invalid password or the encrypted api key file doesn't exist"
+			print "If you haven't yet generated the encrypted key file, run the encrypt_api_key.py script."
+			while 1:
+				pass
+		
+		self.buff = ""
+		self.timeout = 15
+		self.__url_parts = urlparse.urlsplit("https://mtgox.com/api/0/")
+		self.clock_window = time.time()
+		self.clock = time.time()
+		self.query_count = 0
+		self.query_limit_per_time_slice = 5
+		self.query_time_slice = 10
+
+	def throttle(self):
+		self.clock = time.time()
+		tdelta = self.clock - self.clock_window
+		if tdelta > self.query_time_slice:
+			self.query_count = 0
+			self.clock_window = time.time()
+		self.query_count += 1
+		if self.query_count > self.query_limit_per_time_slice:
+			#throttle the connection
+			#print "### Throttled ###"
+			time.sleep(self.query_time_slice - tdelta)
+		return
+       
+	def perform(self, path, params):
+		self.throttle()
+		nonce =  str(int(time.time()*1000))
+		if params != None:
+			params = params.items()
+		else:
+			params = []
+
+		params += [(u'nonce',nonce)]
+		post_data = urllib.urlencode(params)
+		ahmac = base64.b64encode(str(hmac.new(base64.b64decode(self.secret),post_data,hashlib.sha512).digest()))
+
+		# Create header for auth-requiring operations
+		header = {
+			"User-Agent": 'ga-bitbot',
+			"Rest-Key": self.key,
+			"Rest-Sign": ahmac
+			}
+
+		url = urlparse.urlunsplit((
+			self.__url_parts.scheme,
+			self.__url_parts.netloc,
+			self.__url_parts.path + path,
+			self.__url_parts.query,
+			self.__url_parts.fragment
+		))	
+		
+		req = urllib2.Request(url, post_data, header)
+
+		with closing(urllib2.urlopen(req, post_data)) as res:
+			data = json.load(res)
+
+		if u"error" in data:
+			if data[u"error"] == u"Not logged in.":
+				raise UserError()
+			else:
+				raise ServerError(data[u"error"])
+		else:
+			return data
+		
+
+	def request(self, path, params):
+		ret = self.perform(path, params)
+		if "error" in ret:
+			raise UserError(ret["error"])
+		else:
+			return ret
+
+	#public api
+	def get_info(self):
+		return self.request('info.php', None) 
+	def get_ticker(self):
+		return self.request("ticker.php", None)["ticker"]  
+	def get_depth(self):
+		return self.request("data/getDepth.php", {"Currency":"USD"})
+	def get_trades(self):
+		return self.request("data/getTrades.php", None)
+	def get_balance(self):
+		return self.request("getFunds.php", None)
+	def get_orders(self):
+		return self.request("getOrders.php", None)
+
+	def buy_btc(self, amount, price):
+		if amount < 0.01:
+			print "minimun amount is 0.1btc"
+			return 0
+		params = {"amount":str(amount), "price":str(price)}
+		return self.request("buyBTC.php", params)
+        
+	def cancel_buy_order(self, oid):
+		params = {"oid":str(oid), "type":str(2)}
+		return self.request("cancelOrder.php", params)    
+
+	def cancel_sell_order(self, oid):
+		params = {"oid":str(oid), "type":str(1)}
+		return self.request("cancelOrder.php", params)  
+
+
+if __name__ == "__main__":
+	def ppdict(d):
+		#pretty print a dict
+		print "-"*40
+		for key in d.keys():
+			print key,':',d[key]
+		return d
+
+	def pwdict(d,filename):
+		#pretty write a dict
+		f = open(filename,'w')
+		for key in d.keys():
+			f.write(key + " : " + str(d[key]) + "\n")
+		f.write('\n' + '-'*80 + '\n')
+		f.write(str(d))
+		f.close()
+		return d
+
+	print "\nMTGoxHMAC module test"
+	print "**warning: running this script will initiate then cancel an order on the MtGox exchange.**"
+
+	print "\ndownloaded examples of the MtGox json format will be stored in the test_data folder."
+	c = Client()
+	ppdict(pwdict(c.get_info(),'./test_data/mg_info.txt'))
+	ppdict(pwdict(c.get_depth(),'./test_data/mg_depth.txt'))
+	ppdict(pwdict(c.get_balance(),'./test_data/mg_balance.txt'))
+	d = ppdict(pwdict(c.buy_btc(1.5,0.25),'./test_data/mg_buy.txt'))	
+	ppdict(pwdict(c.get_orders(),'./test_data/mg_orders.txt'))
+	time.sleep(2)
+	ppdict(pwdict(c.cancel_buy_order(d['oid']),'./test_data/mg_cancel_buy.txt'))
+	print "done."
+
+
--- encrypt_api_key.py
+++ encrypt_api_key.py
@@ -1,6 +1,8 @@
 from Crypto.Cipher import AES
 import hashlib
 import json
+import time
+import random
 
 print "\n\nga-bitbot API Key Encryptor v0.1a"
 print "-" * 30
@@ -16,23 +18,37 @@ print "\n\nEnter an encryption password:"
 print "(This is the password ga-bitbot will require to execute trades)"
 password = raw_input()
 
+
+print "\n"
+print "Generating the local password salt..."
+pre_salt = str(time.time() * random.random() * 1000000) + 'H7gfJ8756Jg7HBJGtbnm856gnnblkjiINBMBV734'
+salt = hashlib.sha512(pre_salt).digest()
+f = open('./config/salt.txt','w')
+f.write(salt)
+f.close()
+
+
 print "\n"
 print "Generating the encrypted API KEY file..."
-hash_pass = hashlib.sha256(password).digest()
+hash_pass = hashlib.sha256(password + salt).digest()
 encryptor = AES.new(hash_pass, AES.MODE_CBC)
 text = json.dumps({"key":key,"secret":secret})
 #pad the text
 pad_len = 16 - len(text)%16
 text += " " * pad_len
 ciphertext = encryptor.encrypt(text)
-f = open('api_key.txt','w')
+f = open('./config/api_key.txt','w')
 f.write(ciphertext)
 f.close()
 
 print "Verifying encrypted file..."
-f = open('api_key.txt','r')
+f = open('./config/api_key.txt','r')
 d = f.read()
 f.close()
+f = open('./config/salt.txt','r')
+salt = f.read()
+f.close()
+hash_pass = hashlib.sha256(password + salt).digest()
 decryptor = AES.new(hash_pass, AES.MODE_CBC)
 text = decryptor.decrypt(d)
 d = json.loads(text)
