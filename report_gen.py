fa5d8a581dcac9d6e9dadff3e4d7687251d28de4
emfb <emfb@monolith.(none)>
2011-12-16 19:13:06 -0500

    updated report_gen max_days variable to match gts.py. bug report created to consolidate shared variables into an outside configuration file.

--- report_gen.py
+++ report_gen.py
@@ -50,26 +50,28 @@ print "Connected to",__server__,":",__port__
 from bct import *
 
 
-max_length = 60 * 24 * 360
+max_length = 60 * 24 * 60
 
 def load():
-    #open the history file
-    f = open("./datafeed/bcfeed_mtgoxUSD_1min.csv",'r')
-    #f = open("./datafeed/test_data.csv",'r')
-    d = f.readlines()
-    f.close()
-    
-    if len(d) > max_length:
-	#truncate the dataset
-	d [max_length * -1:]
+	#open the history file
+	#print "loading the data set"
+	f = open("./datafeed/bcfeed_mtgoxUSD_1min.csv",'r')
+	#f = open("./datafeed/test_data.csv",'r')
+	d = f.readlines()
+	f.close()
 
-    #load the backtest dataset
-    input = []
-    for row in d[1:]:
-	r = row.split(',')[1] #last price
-	t = row.split(',')[0] #time
-	input.append([int(float(t)),float(r)])
-    return input
+	if len(d) > max_length:
+		#truncate the dataset
+		d = d[max_length * -1:]
+
+	#load the backtest dataset
+	input = []
+	for row in d[1:]:
+		r = row.split(',')[1] #last price
+		t = row.split(',')[0] #time
+		input.append([int(float(t)),float(r)])
+	#print "done loading:", str(len(input)),"records."
+	return input
 
 
 while 1:
