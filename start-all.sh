1acc8172abf5820de1ce25c9891a4bee58b8104a
Brian Monkaba <brian.monkaba@gmail.com>
2013-02-02 14:34:49 -0500

    added start up scripts for all, client only and server only configurations added redis.conf which disables writing db snapshots

---
+++ start-all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash          
+cd config
+redis-server redis.conf &
+cd ..
+pypy gal.py all &
+sleep 5
+cd tools
+cd nimbs
+node server.js &
+cd ..
+cd ..
+sleep 3
+python -mwebbrowser http://127.0.0.1:8088/#tab-10 &
+pypy bid_maker.py
+
